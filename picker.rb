57528910549dff74253cf3f05d1ae18e15bfe3ed
gobkung01 <gobkung01@hotmail.co.th>
2014-10-23 08:13:31 -0300

    Update picker.rb
    ...

--- picker.rb
+++ picker.rb
@@ -4,7 +4,7 @@ def pick_engineer() #Method to pick name from file
   name_file = File.open("engineers.txt") #Open file text.
   name_list = Array.new() #Create blank array.
   name_file.each { |name| name_list.push(name) } #Read name from file then push it to array.
-  puts name_list[rand(name.length)] #Random name from list then print it.
+  puts name_list[rand(name_list.length)] #Random name from list then print it.
 end
 
 pick_engineer() #Call method
